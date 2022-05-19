02c3d92cc0086843e2e8935d0bfa3056e1138ed2
chitrapu raju <chitrapu.raju.20@mountblue.tech>
2022-05-20 01:19:16 +0530

    first question solved

--- MyProject.java
+++ MyProject.java
@@ -1,15 +1,34 @@
 import java.io.*;
 import java.util.*;
 public class MyProject {
+    static HashMap<Integer , Integer> noOfMatches = new HashMap<>();
     public static void main(String [] args){
         try{
             File fileObj = new File("./matches.csv");
             Scanner readObj = new Scanner(fileObj);
-            System.out.println("File opened successfully");
+            //System.out.println("File opened successfully");
+            boolean firstLine = true;
+            while(readObj.hasNextLine()) {
+                String line = readObj.nextLine();
+                if (firstLine) {
+                    firstLine = false;
+                    continue;
+                }
+                String [] data = line.split(",");
+                //System.out.println(Integer.parseInt(data[0]));
+                if(noOfMatches.containsKey(Integer.valueOf(data[1]))){
+                    noOfMatches.put(Integer.valueOf((data[1])), noOfMatches.get(Integer.valueOf((data[1])))+1);
+                }
+                else {
+                    noOfMatches.put(Integer.valueOf(data[1]), 1);
+                }
+            }
+            readObj.close();
         }
         catch(FileNotFoundException e){
             System.out.println("File Not Found");
             e.getMessage();
         }
+        System.out.println(noOfMatches);
     }
 }
